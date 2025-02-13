8998fab7a411a4f8c52133190deffba44d11184d
japnitbbbb <27brarj2@retsdstudent.ca>
2025-02-13 20:48:03 +0000

    doneee

--- src/assignment.py
+++ src/assignment.py
@@ -5,7 +5,11 @@
 num_str_1 = "42"
 num_str_2 = "13"
 num_str_3 = "7"
-word_1 = "robots"
-word_2 = "built"
-word_3 = "today"
-word_4 = "were"
+word_1 = " robots"
+word_2 = " built"
+word_3 = " today"
+word_4 = " were"
+num_1 = int(num_str_1)
+num_2 = int(num_str_2)
+result = str(int(num_1) + int(num_2)) + word_1 + word_4 + word_2 + word_3
+sentence = str(int(num_1) + int(num_2)) + word_1 + word_4 + word_2 + word_3
